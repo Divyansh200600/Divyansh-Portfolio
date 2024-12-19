0b56c9e1ed562006a66331a28f59fa6dd8f217aa
Divyansh Chauhan <divyansh20060@gmail.com>
2024-12-19 20:14:08 +0530

    Portfolio is done only projects lefts

--- Script/script.js
+++ Script/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["frontend development", "backend development", "web designing",  "web development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
@@ -84,7 +84,7 @@ async function fetchData(type = "skills") {
     type === "skills" ?
         response = await fetch("Script/skills.json")
         :
-        response = await fetch("./projects/projects.json")
+        response = await fetch("./Script/projects.json")
     const data = await response.json();
     return data;
 }
